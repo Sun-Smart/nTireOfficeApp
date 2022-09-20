696f5f7a95d118dcb75ad44be2bbb7277c37c4c7
NandhiniSunsmart <113329666+NandhiniSunsmart@users.noreply.github.com>
2022-09-20 15:04:41 +0530

    Sub header heading text color changed
    Sub header heading text color changed

--- nTireofficeApp/src/app/Hrms_pages/od-request/od-request.module.ts
+++ nTireofficeApp/src/app/Hrms_pages/od-request/od-request.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { DatePipe } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 import { OdRequestPageRoutingModule } from './od-request-routing.module';
@@ -15,6 +15,7 @@ import { OdRequestPage } from './od-request.page';
     IonicModule,
     OdRequestPageRoutingModule
   ],
+  providers: [DatePipe],
   declarations: [OdRequestPage]
 })
 export class OdRequestPageModule {}
