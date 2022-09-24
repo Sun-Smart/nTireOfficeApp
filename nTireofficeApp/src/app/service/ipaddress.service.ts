b4b185c866b903aba194ed3ed121fa0c7b36ce10
Kalaivanan <kalaivanan@sunsmart.co.in>
2022-09-24 13:07:29 +0530

    HRMS Changes
    Hrms dashboard issue,font changes

--- nTireofficeApp/src/app/service/ipaddress.service.ts
+++ nTireofficeApp/src/app/service/ipaddress.service.ts
@@ -30,12 +30,12 @@ export class IpaddressService {
     //   this.ipaddress1 = "http://65.0.146.170";
     //  this.ipaddress = "http://65.0.146.170:8034";
 
-    // this.ipaddress1 = "http://prod.herbie.ai";
-    // this.ipaddress ="https://prod.herbie.ai:8033";
+    this.ipaddress1 = "http://prod.herbie.ai";
+    this.ipaddress ="https://prod.herbie.ai:8033";
 
 
-    this.ipaddress1 = "http://demo.herbie.ai";
-    this.ipaddress = "https://demo.herbie.ai:8033";
+    // this.ipaddress1 = "http://demo.herbie.ai";
+    // this.ipaddress = "https://demo.herbie.ai:8033";
 
     // ICRISAT
     // this.ipaddress = "http://192.168.0.154:8033";
@@ -43,29 +43,29 @@ export class IpaddressService {
     //  this.ipaddress1="http://192.168.0.154";
 
 
-    // this.serviceurlhrms = "/SSGmobileapi/HRMS/HRMS.svc/";
-    // this.serviceurlhrms1 = "/SSGmobileapi/bo/bo.svc/";
-    // this.serviceurlhrms2 = "/dms/DMS/cams_hrms_sales/";
-    // this.serviceurlhrms3 = "/SSGmobileapi/BO/BO.svc/";
-    // this.serviceurlhrms4 = "/dms/DMS/dmsapi/DMS/";
-    // this.serviceurlhrms5 = "/dms/DMS/E-attendence/";
-
-
-
-    this.serviceurlhrms = "/nTireMobileCoreAPI/api/HRMS";
-    this.serviceurlhrms1 = "/nTireMobileCoreAPI/api/HRMS/";
-    this.serviceurlhrms2 = "/nTireMobileCoreAPI/api/HRMS/";
-    this.serviceurlhrms3 = "/nTireMobileCoreAPI/api/HRMS/";
+    this.serviceurlhrms = "/SSGmobileapi/HRMS/HRMS.svc/";
+    this.serviceurlhrms1 = "/SSGmobileapi/bo/bo.svc/";
+    this.serviceurlhrms2 = "/dms/DMS/cams_hrms_sales/";
+    this.serviceurlhrms3 = "/SSGmobileapi/BO/BO.svc/";
     this.serviceurlhrms4 = "/dms/DMS/dmsapi/DMS/";
     this.serviceurlhrms5 = "/dms/DMS/E-attendence/";
 
 
+
+    // this.serviceurlhrms = "/nTireMobileCoreAPI/api/HRMS";
+    // this.serviceurlhrms1 = "/nTireMobileCoreAPI/api/HRMS/";
+    // this.serviceurlhrms2 = "/nTireMobileCoreAPI/api/HRMS/";
+    // this.serviceurlhrms3 = "/nTireMobileCoreAPI/api/HRMS/";
+    // this.serviceurlhrms4 = "/dms/DMS/dmsapi/DMS/";
+    // this.serviceurlhrms5 = "/dms/DMS/E-attendence/";
+
+
     // Sales
-    this.serviceurl = "/nTireMobileCoreAPI/api/Sales/";
-    this.serviceurlSales = "/nTireMobileCoreAPI/api/Sales/";
-    
-    // this.serviceurl = "/SSGmobileapi/LMS/LMS.svc/";
-    // this.serviceurlSales = "/dms/DMS/sales/";
+    // this.serviceurl = "/nTireMobileCoreAPI/api/Sales/";
+    // this.serviceurlSales = "/nTireMobileCoreAPI/api/Sales/";
+
+    this.serviceurl = "/SSGmobileapi/LMS/LMS.svc/";
+    this.serviceurlSales = "/dms/DMS/sales/";
 
     this.getLoginLink = '/dms';
 
@@ -73,12 +73,12 @@ export class IpaddressService {
 
 
     //Cams
-    
-    // this.serviceurlCamsNode = "/dms/DMS/cams_hrms_sales"
-    // this.serviceurlCams = "/SSGmobileapi/cams/cams.svc/"
 
-    this.serviceurlCamsNode = "/nTireMobileCoreAPI/api/Sales"
-    this.serviceurlCams = "/nTireMobileCoreAPI/api/CAMS/"
+    this.serviceurlCamsNode = "/dms/DMS/cams_hrms_sales"
+    this.serviceurlCams = "/SSGmobileapi/cams/cams.svc/"
+
+    // this.serviceurlCamsNode = "/nTireMobileCoreAPI/api/Sales"
+    // this.serviceurlCams = "/nTireMobileCoreAPI/api/CAMS/"
 
     // this.serviceurlCamsNode = "/dms/DMS/cams_hrms_sales"
     // this.serviceurlCams="/mobileapi/cams/cams.svc/"
