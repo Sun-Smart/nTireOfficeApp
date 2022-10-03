b6ea127281bd3daf20e0fec166308c0bedaaa122
suneel-M <suneel@sunsmart.co.in>
2022-10-03 18:07:16 +0530

    new page creation input field adjustment and and vendor quotation nd design in procurement
    new page creation input field adjustment and and vendor quotation nd design in procurement

--- nTireofficeApp/src/app/procurement/vendor-quotation/vendor-quotation.page.ts
+++ nTireofficeApp/src/app/procurement/vendor-quotation/vendor-quotation.page.ts
@@ -11,36 +11,135 @@ import { TableSampleService } from 'src/app/Property_Pages/table-sample.service'
   encapsulation: ViewEncapsulation.None
 })
 export class VendorQuotationPage implements OnInit {
-
+  showedit:boolean=false
   options = { checkboxes: true }
-  data: any = [];
-  columns: any = [];
-  rows: any
+  data: any=[];
+  // columns: any = [];
+  // rows: any
 
-  constructor(private modalCtrl: ModalController, private http:HttpClient, private tableApi : TableSampleService) {
 
-    this.columns = [
-      { name: 'Category', width: "110"  },
+
+  rows = [
+    // { 'category': 'Escape Room', 'qty': '13', 'upprice': '12', 'disc': 'computer',
+    //  'tax1': '876576578', 'tax2': '654678', 'tc' :'50053' ,'basev' :'765788' ,'bidv' :'87688' ,'expdate' :'10/08/1992',
+    //  'delivarydate': '12/02/2020', 'action':'yes' },
+    // { name: 'Dany', gender: 'Male', company: 'KFC' },
+    // { name: 'Molly', gender: 'Female', company: 'Burger King' },
+
+     {
+          "category": "Escape Room",
+          "qty": 12,
+          "upprice": 12,
+          "disc": "computer",
+          "tax1": 876576578,
+          "tax2": 654678,
+          "tc": 50053,
+          "basev": 764578457,
+          "bidv": 5847654,
+          "expdate": "12/09/2022",
+          "delivarydate": "12/09/2022",
+          "action":"edit"
+      },
+      {
+        "category": "Escape Room",
+        "qty": 12,
+        "upprice": 43,
+        "disc": "personal computer",
+        "tax1": 876576578,
+        "tax2": 654678,
+        "tc": 672743,
+        "basev": 3434343,
+        "bidv": 878887,
+        "expdate": "12/09/2022",
+        "delivarydate": "12/09/2022",
+        "action":"edit"
+    }
+  ];
+  columns = [
+    { name: 'Category', width: "110"  },
       { name: 'QTY', width: "120"  },
-      { name: 'UpPrice', width: "110"  },
-      { name: 'disc%', width: "110"  },
+      { name: 'UpPrice', width: "120"  },
+      { name: 'disc%', width: "120"  },
       { name: 'tax1', width: "120"  },
       { name: 'tax2', width: "110"  },
       { name: 'Tc', width: "110"  },
-      { name: 'Base V', width: "120"  },
-      { name: 'Bid V', width: "110"  },
-      { name: 'Exp Date', width: "110"  },
-      { name: 'Delivary Date', width: "120"  },
-    ];
+      { name: 'BaseV', width: "120"  },
+      { name: 'BidV', width: "120"  },
+      { name: 'ExpDate', width: "120"  },
+      { name: 'DelivaryDate', width: "120"  },
+      { name: 'Action', width: "120"  },
+  ];
+  constructor(private modalCtrl: ModalController, private http:HttpClient, private tableApi : TableSampleService) {
+
+    // this.columns = [
+    //   { name: 'Category', width: "110"  },
+    //   { name: 'QTY', width: "120"  },
+    //   { name: 'UpPrice', width: "120"  },
+    //   { name: 'disc%', width: "120"  },
+    //   { name: 'tax1', width: "120"  },
+    //   { name: 'tax2', width: "110"  },
+    //   { name: 'Tc', width: "110"  },
+    //   { name: 'BaseV', width: "120"  },
+    //   { name: 'BidV', width: "120"  },
+    //   { name: 'ExpDate', width: "120"  },
+    //   { name: 'DelivaryDate', width: "120"  },
+    //   { name: 'Action', width: "120"  },
+
+    // ];
+
+
+
+
+
+
+
+
+
+
    }
 
   ngOnInit() {
+    this.data= [
+      {
+          "category": "Escape Room",
+          "qty": 12,
+          "upprice": 12,
+          "disc": "computer",
+          "tax1": 876576578,
+          "tax2": 654678,
+          "tc": 50053,
+          "basev": 764578457,
+          "bidv": 5847654,
+          "expdate": "12/09/2022",
+          "delivarydate": "12/09/2022",
+          "action":"edit"
+      },
+      {
+        "category": "Escape Room",
+        "qty": 12,
+        "upprice": 43,
+        "disc": "personal computer",
+        "tax1": 876576578,
+        "tax2": 654678,
+        "tc": 672743,
+        "basev": 3434343,
+        "bidv": 878887,
+        "expdate": "12/09/2022",
+        "delivarydate": "12/09/2022",
+        "action":"edit"
+    }
+    ]
+    // console.log(this.data)
 
-    this.data = this.tableApi.getData();
-  console.log(this.data)
+    // this.data = this.tableApi.getData();
+  // console.log(this.data)
   }
 
   transCancel(){
     this.modalCtrl.dismiss();
+  }
+
+  edit(){
+    this.showedit=true
   }
 }
