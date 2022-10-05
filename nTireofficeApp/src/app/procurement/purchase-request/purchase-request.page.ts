73a9796b1b04639fa08f34d06bf3c544820e479f
suneel-M <suneel@sunsmart.co.in>
2022-10-05 18:50:47 +0530

    new changes in update PRS status
    new changes in update PRS status

--- nTireofficeApp/src/app/procurement/purchase-request/purchase-request.page.ts
+++ nTireofficeApp/src/app/procurement/purchase-request/purchase-request.page.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import { IpaddressService } from 'src/app/service/ipaddress.service';
 
 @Component({
   selector: 'app-purchase-request',
@@ -10,15 +12,78 @@ export class PurchaseRequestPage implements OnInit {
   showlineItems:boolean=true
   showviewlist:boolean=false
   showfilter:boolean=true
-  constructor(private router :Router) { }
+  getlistitems:any
+
+  userid
+  branchid
+
+
+
+  branch
+  Requisitiondate
+  prscode
+
+  prsmode;
+  reasonpurchase;
+  referenceifany;
+  rfpcomments;
+  order;
+  requestby;
+
+  Description
+  Item
+  Category;
+  qty;
+  unitprice;
+  Requiredbefore
+  netprice;
+  itemdescription
+
+
+  constructor(private router :Router , private httpclient : HttpClient , private Ipaddressservice :IpaddressService) {
+
+    var data ={
+      "Description" :this.Description,
+      "Item" :this.Item,
+      "Category":this.Category,
+      "qty":this.qty,
+      "unitprice": this.unitprice,
+      "Requiredbefore" :this.Requiredbefore,
+      "netprice":this.netprice,
+      "itemdescription":this.itemdescription
+    }
+   }
 
   ngOnInit() {
+
+ this.userid=localStorage.getItem('TUM_USER_ID')
+ this.branchid = localStorage.getItem(' TUM_BRANCH_ID')
+
+
+
   }
 
   showline(){
-    // this.showlineItems=true
+    this.showviewlist=true
     this.showviewlist=true
     this.showlineItems=!this.showlineItems
+
+    var data ={
+      "Description" :this.Description,
+      "Item" :this.Item,
+      "Category":this.Category,
+      "qty":this.qty,
+      "unitprice": this.unitprice,
+      "Requiredbefore" :this.Requiredbefore,
+      "netprice":this.netprice,
+      "itemdescription":this.itemdescription
+    }
+    console.log(data)
+    this.getlistitems =data
+    console.log(this.getlistitems)
+
+    // this.showlineItems=true
+
     // this.showfilter = !this.showfilter;
   }
   submit(){
@@ -30,8 +95,41 @@ export class PurchaseRequestPage implements OnInit {
   close(){
     // window.location.reload()
     // this.router.navigate(['/purchase-request'])
-    // this.showviewlist=true
+    this.showviewlist=true
     this.showlineItems=!this.showlineItems
+
+   var body ={
+
+        "functionid":"1",
+        "prsid":"",
+        "prscode":"",
+        "status":"0",
+        "createdby":this.userid, //userid
+        "ipaddress":"0",
+        "reasonpurchase":this.reasonpurchase,
+        "netamount":this.netprice,
+        "currency":"0",
+        "requestcomments":"0",
+        "isbid":"0",
+        "prstype":"0",
+        "branchid":this.branchid,//TUM_BRANCH_ID
+        "prsref":"0",
+        "userid":this.userid,//userid
+        "requestby":this.userid,//userid
+        "requestdate":"",
+        "requettype":this.prsmode,
+        "issinglevendor":"",
+        "orderpriority":""
+
+    }
+
+    this.httpclient.post(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceerpapi + 'get_PRS_Insert_Update', body).subscribe((res: any) => {
+      // this.getresponse = res;
+      console.log(res)
+})
+
+
   }
+
 
 }
