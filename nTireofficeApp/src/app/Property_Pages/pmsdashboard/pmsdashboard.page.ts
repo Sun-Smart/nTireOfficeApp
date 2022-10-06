019ea128df3ba35f1625b8d57ed58233d2c4a18e
Muthusamy <muthumaya.12@gmail.com>
2022-10-06 16:19:52 +0530

    pms
    table added

--- nTireofficeApp/src/app/Property_Pages/pmsdashboard/pmsdashboard.page.ts
+++ nTireofficeApp/src/app/Property_Pages/pmsdashboard/pmsdashboard.page.ts
@@ -5,7 +5,6 @@ import { Chart } from 'chart.js';
 import { Platform } from '@ionic/angular';
 import { TableSampleService } from '../table-sample.service';
 import { ViewEncapsulation } from '@angular/core';
-import { IonLabel } from '@ionic/core/components';
 
 @Component({
   selector: 'app-pmsdashboard',
@@ -43,23 +42,22 @@ export class PmsdashboardPage implements OnInit {
   constructor(private http: HttpClient, private platform: Platform, public Ipaddressservice: IpaddressService, private tableApi: TableSampleService) {
     this.userid = window.localStorage['TUM_USER_ID'];
     this.username = localStorage.getItem('TUM_USER_NAME');
-    this.getCategoryCountChart();
+    // this.getCategoryCountChart();
     this.getBranchCountChart();
-    this.getEmployeeCountChart();
 
     this.columns = [
-      { name: 'sno', width: "50"},
-      { name: 'invoice', width: "90" },
-      { name: 'month', width: "90" },
-      { name: 'amount', width: "70"},
-      { name: 'status', width: "90"},
+      { name: 'sno',},
+      { name: 'invoice', },
+      { name: 'month', },
+      { name: 'amount', },
+      { name: 'status', },
     ];
 
     this.columnsStatus = [
-      { name: 'sno',},
-      { name: 'readBy', },
-      { name: 'description', },
-      { name: 'status', },
+      { name: 'issuecode',},
+      { name: 'issuedate', },
+      { name: 'issuedescription', },
+      { name: 'Status', },
     ];
 
     this.columnsDetails = [
@@ -90,8 +88,17 @@ export class PmsdashboardPage implements OnInit {
     this.data = this.tableApi.getDashbTable1();
     console.log(this.data);
 
-    this.dataStatus = this.tableApi.getDashbTable2();
-    console.log(this.dataStatus);
+
+
+this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlProperty + 'issuestatus?strfunction=1&branch=1&userid=1').subscribe((res:any)=>{
+console.log(res)
+this.dataStatus=res
+
+
+})
+
+    // this.dataStatus = this.tableApi.getDashbTable2();
+    // console.log(this.dataStatus);
 
     this.dataDetails = this.tableApi.getDashbTable3();
     console.log(this.dataDetails);
@@ -104,156 +111,64 @@ export class PmsdashboardPage implements OnInit {
 
   }
 
-  getCategoryCountChart = function () {
-    debugger;
-    var sourcearray = [];
-    const header = new Headers();
-    header.append("Content-Type", "application/json");
+  // getCategoryCountChart = function () {
+  //   debugger;
+  //   var sourcearray = [];
+  //   const header = new Headers();
+  //   header.append("Content-Type", "application/json");
 
-    let options = new HttpHeaders().set('Content-Type', 'application/json');     
-    this.http.get(this.Ipaddressservice.ipaddress1 +this.Ipaddressservice.serviceurlProperty+'customerpayments?strfunction=1&branch=1&userid=1').subscribe(resp => {
-      debugger
-      this.result = resp;
+  //   let options = new HttpHeaders().set('Content-Type', 'application/json');     
+  //   this.http.get(this.Ipaddressservice.ipaddress1 +this.Ipaddressservice.serviceurlProperty+'customerpayments?strfunction=1&branch=1&userid=1').subscribe(resp => {
+  //     debugger
+  //     this.result = resp;
 
-      this.CategoryCount = this.result;
-      console.log(this.CategoryCount);
-      this.labels2 = [];
-      this.data2 = [];
-      for (var i = 0; i < this.CategoryCount.length; i++) {
-        // $scope.labels2[i] = $scope.CategoryCount[i].AssetCategory + '-' + $scope.CategoryCount[i].AssetCount;
-        this.labels2[i] = "Invoice="+this.CategoryCount[i].Invoice + " "+ "OutStanding=" + this.CategoryCount[i].OutStanding + " "+"Paid="+ this.CategoryCount[i].Paid;
-        this.data2[i] = this.CategoryCount[i].Year;
-      }
-      console.log(this.labels2);
-      console.log(this.data2);
-      const canvas = <HTMLCanvasElement>document.getElementById('myChart');
-      const ctx = canvas.getContext('2d');
-      // var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-          labels: this.labels2,
-          datasets: [{
-            label: this.labels2,
-            data: this.data2,
-            backgroundColor: ['rgba(247,70,74,1)', 'rgba(220,220,220,1)', 'rgba(151,187,205,1)', 'rgba(70,191,189,1)', 'rgba(253,180,92,1)', 'rgba(148,159,177,1)', 'rgba(77,83,96,1)', 'rgba(103,16,103,1)', 'rgba(165,131,134,1)', '#FF4500', '#800000', '#00BFFF ', '#000000', '#00FF00', '#008080', '#FF00FF']
+  //     this.CategoryCount = this.result;
+  //     console.log(this.CategoryCount);
+  //     this.labels2 = [];
+  //     this.data2 = [];
+  //     for (var i = 0; i < this.CategoryCount.length; i++) {
+  //       // $scope.labels2[i] = $scope.CategoryCount[i].AssetCategory + '-' + $scope.CategoryCount[i].AssetCount;
+  //       this.labels2[0] = this.CategoryCount[1].Invoice + ' - ' + this.CategoryCount[1].OutStanding +' - ' + this.CategoryCount[1].Paid;
+  //       this.data2[0] = this.CategoryCount[1].Year;
+  //     }
+  //     console.log(this.labels2);
+  //     console.log(this.data2);
+  //     const canvas = <HTMLCanvasElement>document.getElementById('myChart');
+  //     const ctx = canvas.getContext('2d');
+  //     // var ctx = document.getElementById("myChart");
+  //     var myChart = new Chart(ctx, {
+  //       type: 'pie',
+  //       data: {
+  //         labels: this.labels2,
+  //         datasets: [{
+  //           label: this.labels2,
+  //           data: this.data2,
+  //           backgroundColor: ['rgba(247,70,74,1)', 'rgba(220,220,220,1)', 'rgba(151,187,205,1)', 'rgba(70,191,189,1)', 'rgba(253,180,92,1)', 'rgba(148,159,177,1)', 'rgba(77,83,96,1)', 'rgba(103,16,103,1)', 'rgba(165,131,134,1)', '#FF4500', '#800000', '#00BFFF ', '#000000', '#00FF00', '#008080', '#FF00FF']
 
-          }]
-        },
-        options: {
-          responsive: true,
-          fullwidth: true,
-          legend: {
-            display: true,
-            align: 'start',
-            position: 'bottom',
-            verticalAlign: "center",
-            labels: {
-              boxWidth: 20,
-              padding: 20
-            }
-          }
-        }
-      });
+  //         }]
+  //       },
+  //       options: {
+  //         responsive: true,
+  //         fullwidth: true,
+  //         legend: {
+  //           display: true,
+  //           align: 'start',
+  //           position: 'bottom',
+  //           verticalAlign: "center",
+  //           labels: {
+  //             boxWidth: 20,
+  //             padding: 20
+  //           }
+  //         }
+  //       }
+  //     });
 
-    }, error => {
-
-
-    });
-
-  }
+  //   }, error => {
 
 
-  getEmployeeCountChart = function () {
-    debugger;
-    var sourcearray = [];
-    const header = new Headers();
-    header.append("Content-Type", "application/json");
+  //   });
 
-    let options = new HttpHeaders().set('Content-Type', 'application/json');     
-    this.http.get(this.Ipaddressservice.ipaddress1 +this.Ipaddressservice.serviceurlProperty+'emloyeemaintenance?strfunction=1&branch=1&userid=1').subscribe(resp => {
-      debugger
-      this.result = resp;
-
-      this.employeeCount = this.result;
-      console.log(this.employeeCount);
-      // this.Total = new IonLabel;
-      // this.Compplete =new IonLabel;
-      // this.Pending =new IonLabel;
-      this.labels2 = [];
-      this.data2 = [];
-      for (var i = 0; i < this.employeeCount.length; i++) {
-        debugger
-
-      this.FieldName = this.employeeCount[i].FieldName;
-   
-      if(this.FieldName= "Total" )
-      {
-       // this. labels2[0] ="Total";
-        this. labels2[0] ="Total";
-        this. data2[0] =  this.employeeCount[i].Total ;
-      }
-      if(this.FieldName="Complete" )
-      {
-        //this. labels2[1] ="Complete";
-        this. labels2[1] ="Complete";
-        this. data2[1] =  this.employeeCount[i].Completed ;
-      }
-      if(this.FieldName="Pending" )
-      {
-        //this. labels2[2] ="Pending";
-        this. labels2[2] ="Pending";
-        this. data2[2] =  this.employeeCount[i].Pending ;
-      }
-        // $scope.labels2[i] = $scope.CategoryCount[i].AssetCategory + '-' + $scope.CategoryCount[i].AssetCount;
-      //   this.labels2[i] ="Total,Completed,Pending" ;//"Total="+this.employeeCount[i].Total + " "+ "Completed=" + this.employeeCount[i].Completed + " "+"Pending="+ this.employeeCount[i].Pending;
-      //   this.data2[i] = "Total =" + this.employeeCount[i].Completed +this.employeeCount[i].Pending;
-      //  this.data2[i] = "Completed =" + this.employeeCount[i].Completed ;
-      //   this.data2[i] =  "Pending ="+this.employeeCount[i].Pending;        
-        
-      }
-
-      console.log(this.labels2);
-      console.log(this.data2);
-      const canvas = <HTMLCanvasElement>document.getElementById('myChart1');
-      const ctx = canvas.getContext('2d');
-      // var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-          labels: this.labels2,
-          datasets: [{
-            label: this.labels2,
-            data: this.data2,
-            backgroundColor: ['rgba(247,70,74,1)', 'rgba(220,220,220,1)', 'rgba(151,187,205,1)', 'rgba(70,191,189,1)', 'rgba(253,180,92,1)', 'rgba(148,159,177,1)', 'rgba(77,83,96,1)', 'rgba(103,16,103,1)', 'rgba(165,131,134,1)', '#FF4500', '#800000', '#00BFFF ', '#000000', '#00FF00', '#008080', '#FF00FF']
-
-          }]
-        },
-        options: {
-          responsive: true,
-          fullwidth: true,
-          legend: {
-            display: true,
-            align: 'start',
-            position: 'bottom',
-            verticalAlign: "center",
-            labels: {
-              boxWidth: 20,
-              padding: 20
-            }
-          }
-        }
-      });
-
-    }, error => {
-
-
-    });
-
-  }
-
-
-
+  // }
 
   getBranchCountChart = function () {
     debugger
@@ -274,7 +189,7 @@ export class PmsdashboardPage implements OnInit {
         this.labels = [];
         this.data = [];
         for (var i = 0; i < this.branchCount.length; i++) {
-          this.labels[i] = this.branchCount[i].Property_Status;
+          this.labels[i] = this.branchCount[i].Property_Status + ' - ' + this.branchCount[i].OutStanding +' - ' + this.branchCount[i].Paid;
         this.data[i] = this.branchCount[i].No_of_Property;
           // $scope.data.push($scope.colorpie[i].color);
       }
@@ -285,7 +200,7 @@ export class PmsdashboardPage implements OnInit {
             {
     
               data: this.data,
-              backgroundColor: ['rgb(16, 99, 16)','rgb(68, 49, 9)','rgb(0,128,128)'],
+              backgroundColor: ['rgb(16, 99, 16)','rgb(68, 49, 9)'],
     
     
             }]
@@ -297,7 +212,7 @@ export class PmsdashboardPage implements OnInit {
             type: 'bar',
     
             data: {
-              labels: this.labels,
+              labels: this.label,
               datasets: sourcearray,
     
             },
