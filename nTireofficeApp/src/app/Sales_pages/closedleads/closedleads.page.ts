1c7367038873b614452c1e1c1c6d1cfe453b2fea
Kalaivanan <kalaivanan@sunsmart.co.in>
2022-10-07 17:50:24 +0530

    Font Change Pending leads
    Font Change Pending leads

--- nTireofficeApp/src/app/Sales_pages/closedleads/closedleads.page.ts
+++ nTireofficeApp/src/app/Sales_pages/closedleads/closedleads.page.ts
@@ -578,6 +578,7 @@ export class ClosedleadsPage implements OnInit, OnDestroy {
           this.pendleaddetails[i].RESPHONE = '-';
         }
         if (this.pendleaddetails[i].CreatedOn != undefined) {
+          debugger;
 
           var date = this.pendleaddetails[i].CreatedOn1;
 
