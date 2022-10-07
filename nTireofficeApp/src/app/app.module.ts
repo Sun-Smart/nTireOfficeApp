08ebf25384a3c2a1f075c29e34bda85ee1dc6218
Samundeeswari-Pandian <113327771+Samundeeswari-Pandian@users.noreply.github.com>
2022-10-07 17:53:26 +0530

    App Component => Link changes
    App Component => Link changes

--- nTireofficeApp/src/app/app.module.ts
+++ nTireofficeApp/src/app/app.module.ts
@@ -17,12 +17,12 @@ import { CommonModule } from '@angular/common';
 
 // import { ChartsModule } from 'chartjs';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpClientModule, HttpClientXsrfModule, HTTP_INTERCEPTORS   } from '@angular/common/http';
-import { AppComponent } from './app.component';
+import { AppComponent } from '././app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, 
-    IonicModule.forRoot(), 
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
     NgxDatatableModule,
