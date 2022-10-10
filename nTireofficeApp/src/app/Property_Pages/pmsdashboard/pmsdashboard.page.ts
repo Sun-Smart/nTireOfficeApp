01cebee577f2c9a790182cf512759dfb7415253d
Muthusamy <muthumaya.12@gmail.com>
2022-10-10 18:12:50 +0530

    pms dashboard
    bar chart

--- nTireofficeApp/src/app/Property_Pages/pmsdashboard/pmsdashboard.page.ts
+++ nTireofficeApp/src/app/Property_Pages/pmsdashboard/pmsdashboard.page.ts
@@ -22,17 +22,17 @@ export class PmsdashboardPage implements OnInit {
 
   options = { checkboxes: true }
   data: any = [];
-  dataStatus : any =[];
-  dataDetails : any =[];
-  dataVaccant : any =[];
-  dataRaised : any =[];
+  dataStatus: any = [];
+  dataDetails: any = [];
+  dataVaccant: any = [];
+  dataRaised: any = [];
 
 
   columns: any = [];
   columnsStatus: any = [];
   columnsDetails: any = [];
   columnsVaccant: any = [];
-  columnsRaised : any = [];
+  columnsRaised: any = [];
   rows: any
   barChart: any;
 
@@ -53,14 +53,14 @@ export class PmsdashboardPage implements OnInit {
 
 
     this.columnsStatus = [
-      { name: 'issuecode',},
+      { name: 'issuecode', },
       { name: 'issuedate', },
       { name: 'issuedescription', },
       { name: 'Status', },
     ];
 
     this.columnsDetails = [
-      { name: 'sno',},
+      { name: 'sno', },
       { name: 'propertycode', },
       { name: 'issuedescription', },
       { name: 'status', },
@@ -74,12 +74,12 @@ export class PmsdashboardPage implements OnInit {
 
 
 
-this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlProperty + 'issuestatus?strfunction=1&branch=1&userid=1').subscribe((res:any)=>{
-console.log(res)
-this.dataStatus=res
+    this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlProperty + 'issuestatus?strfunction=1&branch=1&userid=1').subscribe((res: any) => {
+      console.log(res)
+      this.dataStatus = res
 
 
-})
+    })
 
     // this.dataStatus = this.tableApi.getDashbTable2();
     // console.log(this.dataStatus);
@@ -110,57 +110,53 @@ this.dataStatus=res
       this.customerPaymenhtresult = resp;
 
       this.customerPayment = this.customerPaymenhtresult;
+      console.log(this.customerPayment)
       console.log(this.customerPayment[0].Invoice);
       this.labels = [];
       this.data = [];
 
       for (var i = 0; i < this.customerPayment.length; i++) {
         this.labels[i] = this.customerPayment[i].Year;
-        // this.FieldName1 = this.customerPayment[i].Invoice;
-        // this.FieldName2 = this.customerPayment[i].OutStanding;
-        // this.FieldName3 = this.customerPayment[i].Paid
-        this.data[i] = 'Invoice -' + this.customerPayment[i].Invoice + ', ' + 'OutStanding -' + this.customerPayment[i].OutStanding + ', ' + 'Paid -' +  this.customerPayment[i].Paid;
-        // $scope.data.push($scope.colorpie[i].color);
-      }
-console.log(this.data);
 
-    //   console.log(this.labels);
-    //   console.log(JSON.stringify(this.data[0]));
-    //  var  test =JSON.stringify(this.data[0]);
-    //  test = JSON.parse(test);
-    //  test = JSON.parse(test);
-    //  console.log(test);
+        this.data[i] = this.customerPayment[i].Invoice + this.customerPayment[i].OutStanding + this.customerPayment[i].Paid;
+      }
+      console.log(this.data);
       if (this.barchart) {
         this.barchart.destroy();
       }
-
       this.barchart = new Chart(this.sourcecanvas2.nativeElement, {
         type: 'bar',
         data: {
           labels: this.labels,
           datasets: [{
-            barPercentage: 0.4,
-            barThickness: '20',
+            barPercentage: 0.3,
+            barThickness: '10',
             label: "Invoice",
             stack: "Base",
             backgroundColor: "#E1BA24",
-            data: [this.customerPayment[0].Invoice,this.customerPayment[0].Invoice]
-          }, {
-            barPercentage: 0.4,
-            barThickness: '20',
+            data: [this.customerPayment[0].Invoice, this.customerPayment[0].Invoice]
+
+          }
+            ,
+          {
+            barPercentage: 0.3,
+            barThickness: '10',
             label: "OutStanding",
             stack: "Sensitivity",
             backgroundColor: "#2A93CE",
-            data: [this.customerPayment[0].OutStanding,this.customerPayment[1].OutStanding]
+            data: [this.customerPayment[0].OutStanding, this.customerPayment[1].OutStanding]
+
           },
           {
-            barPercentage: 0.4,
-            barThickness: '20',
+            barPercentage: 0.3,
+            barThickness: '10',
             label: "Paid",
             stack: "solid",
             backgroundColor: "#004073",
-            data: [this.customerPayment[0].Paid,this.customerPayment[1].Paid]
-          }]
+            data: [this.customerPayment[0].Paid, this.customerPayment[1].Paid]
+
+          }
+          ]
         },
         options: {
           legend: {
@@ -181,18 +177,18 @@ console.log(this.data);
               }
             }]
           },
-          tooltips: {
+          // tooltips: {
 
-            callbacks: {
-              title: function (tooltipItem, data) {
-                return data.datasets['labels'][tooltipItem[0]['index']];
-              },
-              label: function (tooltipItem, data) {
-                return data[0]['label'] + " : " + data['datasets'][0]['data'][tooltipItem['index']];
-              },
+          //   callbacks: {
+          //     title: function (tooltipItem, data) {
+          //       return data['labels'][tooltipItem[0]['index']];
+          //     },
+          //     label: function (tooltipItem, data) {
+          //       return data.datasets[0]['label'] + " : " + data['datasets'][0]['data'][tooltipItem['index']];
+          //     },
 
-            },
-          },
+          //   },
+          // },
         }
       })
     });
@@ -205,8 +201,8 @@ console.log(this.data);
     const header = new Headers();
     header.append("Content-Type", "application/json");
 
-    let options = new HttpHeaders().set('Content-Type', 'application/json');     
-    this.http.get(this.Ipaddressservice.ipaddress1 +this.Ipaddressservice.serviceurlProperty+'emloyeemaintenance?strfunction=1&branch=1&userid=1').subscribe(resp => {
+    let options = new HttpHeaders().set('Content-Type', 'application/json');
+    this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlProperty + 'emloyeemaintenance?strfunction=1&branch=1&userid=1').subscribe(resp => {
       debugger
       this.result = resp;
 
@@ -220,32 +216,29 @@ console.log(this.data);
       for (var i = 0; i < this.employeeCount.length; i++) {
         debugger
 
-      this.FieldName = this.employeeCount[i].FieldName;
-   
-      if(this.FieldName= "Total" )
-      {
-       // this. labels2[0] ="Total";
-        this. labels2[0] ="Total";
-        this. data2[0] =  this.employeeCount[i].Total ;
-      }
-      if(this.FieldName="Complete" )
-      {
-        //this. labels2[1] ="Complete";
-        this. labels2[1] ="Complete";
-        this. data2[1] =  this.employeeCount[i].Completed ;
-      }
-      if(this.FieldName="Pending" )
-      {
-        //this. labels2[2] ="Pending";
-        this. labels2[2] ="Pending";
-        this. data2[2] =  this.employeeCount[i].Pending ;
-      }
+        this.FieldName = this.employeeCount[i].FieldName;
+
+        if (this.FieldName = "Total") {
+          // this. labels2[0] ="Total";
+          this.labels2[0] = "Total";
+          this.data2[0] = this.employeeCount[i].Total;
+        }
+        if (this.FieldName = "Complete") {
+          //this. labels2[1] ="Complete";
+          this.labels2[1] = "Complete";
+          this.data2[1] = this.employeeCount[i].Completed;
+        }
+        if (this.FieldName = "Pending") {
+          //this. labels2[2] ="Pending";
+          this.labels2[2] = "Pending";
+          this.data2[2] = this.employeeCount[i].Pending;
+        }
         // $scope.labels2[i] = $scope.CategoryCount[i].AssetCategory + '-' + $scope.CategoryCount[i].AssetCount;
-      //   this.labels2[i] ="Total,Completed,Pending" ;//"Total="+this.employeeCount[i].Total + " "+ "Completed=" + this.employeeCount[i].Completed + " "+"Pending="+ this.employeeCount[i].Pending;
-      //   this.data2[i] = "Total =" + this.employeeCount[i].Completed +this.employeeCount[i].Pending;
-      //  this.data2[i] = "Completed =" + this.employeeCount[i].Completed ;
-      //   this.data2[i] =  "Pending ="+this.employeeCount[i].Pending;        
-        
+        //   this.labels2[i] ="Total,Completed,Pending" ;//"Total="+this.employeeCount[i].Total + " "+ "Completed=" + this.employeeCount[i].Completed + " "+"Pending="+ this.employeeCount[i].Pending;
+        //   this.data2[i] = "Total =" + this.employeeCount[i].Completed +this.employeeCount[i].Pending;
+        //  this.data2[i] = "Completed =" + this.employeeCount[i].Completed ;
+        //   this.data2[i] =  "Pending ="+this.employeeCount[i].Pending;        
+
       }
 
       console.log(this.labels2);
@@ -347,170 +340,173 @@ console.log(this.data);
 
   getBranchCountChart = function () {
     debugger
-      var sourcearray=[];
-      const header = new Headers();
-      header.append("Content-Type", "application/json");
-    
-      let options = new HttpHeaders().set('Content-Type', 'application/json');
-      this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlProperty+'propertystatus?strfunction=1&branch=1&userid=1',{
-        headers: options,
-      }).subscribe((resp:any) => {
-        console.log(resp)
-        debugger
-        this.branchCountresult = resp;
-    
-        this.branchCount = this.branchCountresult;
-        console.log(this.branchCount);
-        this.labels = [];
-        this.data = [];
-        for (var i = 0; i < this.branchCount.length; i++) {
-          this.labels[i] = this.branchCount[i].Property_Status;
-        this.data[i] = this.branchCount[i].No_of_Property;
-          // $scope.data.push($scope.colorpie[i].color);
-      }
-        console.log(this.labels);
-        console.log(this.data);
-    
-           sourcearray = [
-            {
-              data: this.data,
-              backgroundColor: ['rgb(16, 99, 16)','rgb(68, 49, 9)'],
-            }]
-            if (this.sourcechart) {
-              this.sourcechart.destroy();
-          }
-          this.sourcechart = new Chart(this.sourcecanvas1.nativeElement, {
-            type: 'bar',
-            data: {
-              labels: this.labels,
-              datasets: sourcearray,
-            },
-            options: {
-              legend: {
-                display: false
-              },
-              scales: {
-                xAxes: [{
-                  ticks: {
-                    fontSize: 10
-                  }
-                }],
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true,
-                    display: true,
-                    labelString: window.localStorage['TUM_BRANCH_CODE'],
-                  }
-                }]
-              },
-              tooltips: {
-                callbacks: {
-                  title: function (tooltipItem, data) {
-                    return data['labels'][tooltipItem[0]['index']];
-                  },
-                  label: function (tooltipItem, data) {
-                    return "Target" + " : " + data['datasets'][0]['data'][tooltipItem['index']];
-                  },
-                },
-              },
-            }
-          });
-        }, error => {
-        });
-    }
+    var sourcearray = [];
+    const header = new Headers();
+    header.append("Content-Type", "application/json");
 
-
-
-    getToBevaccantChart = function () {
+    let options = new HttpHeaders().set('Content-Type', 'application/json');
+    this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlProperty + 'propertystatus?strfunction=1&branch=1&userid=1', {
+      headers: options,
+    }).subscribe((resp: any) => {
+      console.log(resp)
       debugger
-        var sourcearray=[];
-        const header = new Headers();
-        header.append("Content-Type", "application/json");
-      
-        let options = new HttpHeaders().set('Content-Type', 'application/json');
-        this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlProperty+'tobevaccant?strfunction=1&branch=1&userid=1',{
-          headers: options,
-        }).subscribe((resp:any) => {
-          console.log(resp)
-          debugger
-          this.tovaccantchart = resp;
-      
-          this.tovaccantchartcount = this.tovaccantchart;
-          console.log(this.branchCount);
-          this.labels = [];
-          this.data = [];
-          for (var i = 0; i < this.tovaccantchartcount.length; i++) {
-            this.labels[i] = this.tovaccantchartcount[i].Days;
-          this.data[i] = this.tovaccantchartcount[i].Count;
-            // $scope.data.push($scope.colorpie[i].color);
-        }
-          console.log(this.labels);
-          console.log(this.data);
-      
-             sourcearray = [
-              {
-      
-                data: this.data,
-                backgroundColor: ['rgba(253,180,92,1)', 'rgba(148,159,177,1)', 'rgba(77,83,96,1)', 'rgba(103,16,103,1)', 'rgba(165,131,134,1)', '#FF4500', '#800000', '#00BFFF ',],
-      
-      
-              }]
-              if (this.sourcechart1) {
-                this.sourcechart.destroy();
-            }
-            this.sourcechart1 = new Chart(this.sourcecanvas3.nativeElement, {
-      
-              type: 'bar',
-      
-              data: {
-                labels: this.labels,
-                datasets: sourcearray,
-      
-              },
-              options: {
-                legend: {
-                  display: false
-                },
-                scales: {
-                  xAxes: [{
-                    ticks: {
-                      fontSize: 10
-                    }
-                  }],
-                  yAxes: [{
-                    ticks: {
-                      beginAtZero: true,
-                      display: true,
-                      labelString: window.localStorage['TUM_BRANCH_CODE'],
-      
-                    }
-                  }]
-                },
-                tooltips: {
-      
-                  callbacks: {
-                    title: function (tooltipItem, data) {
-                      return data['labels'][tooltipItem[0]['index']];
-                    },
-                    label: function (tooltipItem, data) {
-                      return "Target" + " : " + data['datasets'][0]['data'][tooltipItem['index']];
-                    },
-      
-                  },
-      
-      
-      
-                },
-              }
-      
-            });
-      
-          }, error => {
-      
-      
-          });
-      
+      this.branchCountresult = resp;
+
+      this.branchCount = this.branchCountresult;
+      console.log(this.branchCount);
+      this.labels = [];
+      this.data = [];
+      for (var i = 0; i < this.branchCount.length; i++) {
+        this.labels[i] = this.branchCount[i].Property_Status;
+        this.data[i] = this.branchCount[i].No_of_Property;
+        // $scope.data.push($scope.colorpie[i].color);
       }
+      console.log(this.labels);
+      console.log(this.data);
+
+      sourcearray = [
+        {
+          data: this.data,
+          backgroundColor: ['rgb(16, 99, 16)', 'rgb(68, 49, 9)'],
+        }]
+      if (this.sourcechart) {
+        this.sourcechart.destroy();
+      }
+      this.sourcechart = new Chart(this.sourcecanvas1.nativeElement, {
+        type: 'bar',
+        data: {
+          labels: this.labels,
+          datasets: sourcearray,
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              ticks: {
+                fontSize: 10
+              }
+            }],
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                display: true,
+                labelString: window.localStorage['TUM_BRANCH_CODE'],
+
+              }
+            }]
+          },
+          // tooltips: {
+
+          //   callbacks: {
+          //     title: function (tooltipItem, data) {
+          //       return data['labels'][tooltipItem[0]['index']];
+          //     },
+          //     label: function (tooltipItem, data) {
+          //       return data.datasets[0]['label'] + " : " + data['datasets'][0]['data'][tooltipItem['index']];
+          //     },
+
+          //   },
+          // },
+        }
+      });
+    }, error => {
+    });
+  }
+
+
+
+  getToBevaccantChart = function () {
+    debugger
+    var sourcearray = [];
+    const header = new Headers();
+    header.append("Content-Type", "application/json");
+
+    let options = new HttpHeaders().set('Content-Type', 'application/json');
+    this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlProperty + 'tobevaccant?strfunction=1&branch=1&userid=1', {
+      headers: options,
+    }).subscribe((resp: any) => {
+      console.log(resp)
+      debugger
+      this.tovaccantchart = resp;
+
+      this.tovaccantchartcount = this.tovaccantchart;
+      console.log(this.branchCount);
+      this.labels = [];
+      this.data = [];
+      for (var i = 0; i < this.tovaccantchartcount.length; i++) {
+        this.labels[i] = this.tovaccantchartcount[i].Days;
+        this.data[i] = this.tovaccantchartcount[i].Count;
+        // $scope.data.push($scope.colorpie[i].color);
+      }
+      console.log(this.labels);
+      console.log(this.data);
+
+      sourcearray = [
+        {
+
+          data: this.data,
+          backgroundColor: ['rgba(253,180,92,1)', 'rgba(148,159,177,1)', 'rgba(77,83,96,1)', 'rgba(103,16,103,1)', 'rgba(165,131,134,1)', '#FF4500', '#800000', '#00BFFF ',],
+
+
+        }]
+      if (this.sourcechart1) {
+        this.sourcechart.destroy();
+      }
+      this.sourcechart1 = new Chart(this.sourcecanvas3.nativeElement, {
+
+        type: 'bar',
+
+        data: {
+          labels: this.labels,
+          datasets: sourcearray,
+
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              ticks: {
+                fontSize: 10
+              }
+            }],
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                display: true,
+                labelString: window.localStorage['TUM_BRANCH_CODE'],
+
+              }
+            }]
+          },
+          tooltips: {
+
+            callbacks: {
+              title: function (tooltipItem, data) {
+                return data['labels'][tooltipItem[0]['index']];
+              },
+              label: function (tooltipItem, data) {
+                return "Target" + " : " + data['datasets'][0]['data'][tooltipItem['index']];
+              },
+
+            },
+
+
+
+          },
+        }
+
+      });
+
+    }, error => {
+
+
+    });
+
+  }
 
 
 }
