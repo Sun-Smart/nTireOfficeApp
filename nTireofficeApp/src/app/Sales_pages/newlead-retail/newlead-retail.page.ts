1b943bd38c1aa2eeccc22c31b127b446849ccc55
Kalaivanan <kalaivanan@sunsmart.co.in>
2022-10-13 14:50:02 +0530

    Hrms Issue Fix
    Hrms Issue Fix

--- nTireofficeApp/src/app/Sales_pages/newlead-retail/newlead-retail.page.ts
+++ nTireofficeApp/src/app/Sales_pages/newlead-retail/newlead-retail.page.ts
@@ -914,7 +914,7 @@ export class NewleadRetailPage implements OnInit {
 
         }
         else {
-          const resultstrarray = this.result.split(" ");
+          const resultstrarray = this.result;
           let lead_id_new = resultstrarray[4];
           lead_id_new = parseInt(lead_id_new);
 
@@ -964,7 +964,7 @@ export class NewleadRetailPage implements OnInit {
             } else {
 
               const passCurrent_locationJSON = {
-                latlong: this.currentlatlon,
+                latlong: parseInt(this.currentlatlon),
                 CustId: this.lead_id,
                 access_token: window.localStorage.token,
                 userid: window.localStorage.TUM_USER_ID,
@@ -1003,7 +1003,7 @@ export class NewleadRetailPage implements OnInit {
                   .subscribe(files => console.log('files', files));
 
                 const objupload = {
-                  pk1: this.lead_id,
+                  pk1: parseInt(this.lead_id),
                   doc_name: this.image[i],
                   doc_desc: this.remarks,
                   doc_path: 'https://demo.herbieai.com/Testntiremydesk/Uploaddocu/SSTPL/' + this.image[i],
@@ -1011,9 +1011,9 @@ export class NewleadRetailPage implements OnInit {
                   FUNCTION_ID: window.localStorage.FUNCTION_ID,
                   file_size: this.file[i].size,
                   access_token: window.localStorage.token,
-                  userid: window.localStorage.TUM_USER_ID,
+                  userid: parseInt(window.localStorage.TUM_USER_ID),
                   usertoken: window.localStorage.usertoken,
-                  USER_ID: window.localStorage.TUM_USER_ID
+                  USER_ID: parseInt(window.localStorage.TUM_USER_ID)
                 };
 
                 //D:/Application/Base Product/nTireOfficeTesting/nTireOffice/Uploaddocu/SSTPL/
