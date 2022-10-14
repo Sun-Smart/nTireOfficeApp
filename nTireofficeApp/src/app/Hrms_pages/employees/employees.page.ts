9984055b9d7476e0cd53cf0c7ba810e39308cbae
Samundeeswari-Pandian <113327771+Samundeeswari-Pandian@users.noreply.github.com>
2022-10-14 12:07:40 +0530

    HRMS => Employee Directory Filter functionality issue
    HRMS => Employee Directory Filter functionality issue cleared

--- nTireofficeApp/src/app/Hrms_pages/employees/employees.page.ts
+++ nTireofficeApp/src/app/Hrms_pages/employees/employees.page.ts
@@ -11,6 +11,7 @@ import { IonicSelectableComponent } from 'ionic-selectable';
 })
 
 export class EmployeesPage implements OnInit {
+  totalemployee1: any;
   displaydetails($event: Event) {
     throw new Error('Method not implemented.');
   }
@@ -102,13 +103,13 @@ export class EmployeesPage implements OnInit {
     console.log("designation:" + this.designation, "branch:" + this.branch, "dept:" + this.department);
 
     if (this.name == undefined || this.name == '') {
-      name = "%20";
+      name = "0";
     } else {
       name = this.name;
     }
 
     if (this.code == undefined || this.code == '') {
-      code = "%20";
+      code = "0";
     } else {
       code = this.code;
     }
@@ -136,31 +137,32 @@ export class EmployeesPage implements OnInit {
     this.length = 0;
 
     this.obj = {
-      // empID: "0",
-      // name: name,
-      // code: code,
-      // designation: designation,
-      // branch: branch,
-      // department: department,
-      // top: this.length,
-      // increment: increment
+      empID: "0",
+      name: name,
+      code: code,
+      designation: designation,
+      branch: branch,
+      department: department,
+      top: this.length,
+      increment: increment,
       //increment: 2000
-      empID: 0,
-      name: 0,
-      code: 0,
-      designation: 0,
-      branch: 0,
-      department: 0,
-      top: 0,
-      increment: 3000,
+      // empID: 0,
+      // name: 0,
+      // code: 0,
+      // designation: 0,
+      // branch: 0,
+      // department: 0,
+      // top: 0,
+      // increment: 3000,
       appURL: 'employeesearch'
+
     }
     console.log(this.obj);
     this.HttpRequest.GetRequest(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlhrms + '/EmployeeSearch/' + this.obj.empID + "/" + this.obj.name + "/" + this.obj.code + "/" + this.obj.designation + "/" + this.obj.branch + "/" + this.obj.department + "/" + this.obj.top + "/" + this.obj.increment + '/' + this.obj.appURL).then(resp => {
       this.loadingdismiss();
       this.loading = false;
       this.allemp = resp;
-      console.log('getb resp', resp)
+      // this.allemp = JSON.parse(resp.toString());
       this.displayEmployee = this.displayEmployee.concat(this.allemp);
       console.log("displayEmployee : " + JSON.stringify(this.displayEmployee));
 
@@ -204,23 +206,24 @@ export class EmployeesPage implements OnInit {
     });
 
     var obj1 = {
-      empID: window.localStorage.getItem("EmployeeID"),
-      name: window.localStorage.getItem("EmployeeName"),
-      code: window.localStorage.getItem("TUM_EMP_CODE"),
-      designation: window.localStorage.getItem("EmpDesignation"),
-      branch: window.localStorage.getItem("TUM_BRANCH_ID"),
-      department: window.localStorage.getItem("EmpDepartment"),
+      empID: "0",
+      name: name,
+      code: code,
+      designation: designation,
+      branch: branch,
+      department: department,
       top: this.length,
       increment: increment,
       appURL: 'employeesearch'
     }
 
-    this.HttpRequest.GetRequest(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlhrms + '/EmployeeSearch/' + obj1.empID + "/" + obj1.name + "/" + obj1.code + "/" + obj1.designation + "/" + obj1.branch + "/" + obj1.department + "/" + obj1.top + "/" + obj1.increment + "/" + obj1.appURL).then(resp => {
+    this.HttpRequest.GetRequest(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceurlhrms + '/EmployeeSearch/' + obj1.empID + "/" + obj1.name + "/" + obj1.code + "/" + obj1.designation + "/" + obj1.branch + "/" + obj1.department + "/" + obj1.top + "/" + obj1.increment + "/" + obj1.appURL).then((resp:any) => {
       console.log(resp)
       this.loading = false;
       // this.totalemployee = JSON.parse(resp.toString()).length;
-      this.totalemployee = JSON.stringify(resp);
-      this.totalemployee = JSON.parse(this.totalemployee);
+      this.totalemployee = resp.length;
+      // this.totalemployee = JSON.parse(this.totalemployee);
+      // this.totalemployee = JSON.parse(resp.toString()).length;
     }, error => {
       this.loading = false;
       // this.loadingdismiss();
