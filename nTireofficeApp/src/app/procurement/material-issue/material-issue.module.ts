dc5b1e65f76ceb9be35270b98fe7caa93af41456
StobartEvansC <113337584+StobartEvansC@users.noreply.github.com>
2022-10-17 14:52:35 +0530

    material issue page update
    material issue page update

--- nTireofficeApp/src/app/procurement/material-issue/material-issue.module.ts
+++ nTireofficeApp/src/app/procurement/material-issue/material-issue.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +14,8 @@ import { MaterialIssuePage } from './material-issue.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    MaterialIssuePageRoutingModule
+    MaterialIssuePageRoutingModule,
+    NgxDatatableModule
   ],
   declarations: [MaterialIssuePage]
 })
