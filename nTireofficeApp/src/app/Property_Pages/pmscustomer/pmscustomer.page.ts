31a1b85a5e6fee529408e5d88959a6163229a7c2
Muthusamy <muthumaya.12@gmail.com>
2022-10-18 18:10:34 +0530

    pms
    changes

--- nTireofficeApp/src/app/Property_Pages/pmscustomer/pmscustomer.page.ts
+++ nTireofficeApp/src/app/Property_Pages/pmscustomer/pmscustomer.page.ts
@@ -18,72 +18,60 @@ export class PmscustomerPage implements OnInit {
   filterTerm: string;
   showfilter: boolean = true;
 
-  propertyBranch:any;
-  propertyLocation:any;
-  Propertyname:any;
-  propertyCode:any;
-  propertyDescription:any;
-  uniqueRef;
-  branchcode: any;
-  branchid: any;
+  propertyBranch: any;
   userId: any;
-
-  strFunctionId
-  strLocationId
-  strPropertyId
-  strPropertyDesc
-  rentelCode
-  strStatus
-  pageIndex
-  pageSize
-  sortExpression
-  alphaname
-  Split_ID
-  strusertype
-
-  userID:any;
-  usertype: any;
-  function: any;
-  branch: any;
-  functionID: any;
-  branchID:any;
-  Location: any;
+  functionID: string;
+  userID: string;
+  usertype: string;
+  accessToken: string;
+  strFunctionId: any;
+  strusertype: any;
   Propertycode: any;
-  assetcode: any;
-  details1 = [];
-  isItemAvailable: boolean;
-  assetcode1: any[];
-  accessToken: any;
   propertyCodeResult: any;
-  propertyCodeResult1: any;
-  userToken: any;
+  params: { access_token: any; usertoken: any; USER_ID: any; };
+  user: any;
+  strBranchId: string;
+  strLocationId: string;
+  strPropertyId: string;
+  strPropertyDesc: string;
+  userid: string;
+  branchid: any;
+  branchID: string;
 
 
 
-  constructor(private modalCtrl: ModalController, 
-    public alertController: AlertController, 
-    private http: HttpClient, 
-    public Ipaddressservice: IpaddressService) 
-    {
-      this.branchID = localStorage.getItem('TUM_BRANCH_ID');
-      this.functionID = localStorage.getItem('FUNCTION_ID');
-      this.branch = localStorage.getItem('TUM_BRANCH_CODE');
-      this.userID = localStorage.getItem('TUM_USER_ID');
-      this.usertype = localStorage.getItem('TUM_USER_TYPE');
-      this.accessToken = localStorage.getItem('token');
-    }
+
+
+
+
+
+  constructor(private modalCtrl: ModalController,
+    public alertController: AlertController,
+    private http: HttpClient,
+    public Ipaddressservice: IpaddressService) {
+    this.branchID = localStorage.getItem('TUM_BRANCH_ID');
+    this.functionID = localStorage.getItem('FUNCTION_ID');
+    this.userID = localStorage.getItem('TUM_USER_ID');
+    this.usertype = localStorage.getItem('TUM_USER_TYPE');
+    this.accessToken = localStorage.getItem('token');
+  }
 
 
   ngOnInit() {
-this.getItems("");
-    this.assetcode = "";
-    this.details1 = [];
+    this.getItems("");
 
-    this.userId = window.localStorage['TUM_USER_ID'],
-    this.branchcode = window.localStorage['TUM_BRANCH_CODE']
-    this.branchid = window.localStorage['TUM_BRANCH_ID']
-    this.strFunctionId = window.localStorage['FUNCTION_ID']
-    this.strusertype = window.localStorage['TUM_USER_TYPE']
+    this.userid = window.localStorage['TUM_USER_ID'],
+    console.log(this.userid);
+    
+    this.strBranchId = window.localStorage['TUM_BRANCH_ID'],
+    console.log(this.strBranchId);
+    
+    this.strFunctionId = window.localStorage['FUNCTION_ID'],
+    console.log(this.strFunctionId);
+    
+    this.strusertype = window.localStorage['TUM_USER_TYPE'],
+    console.log(this.strusertype);
+    
 
   }
   async createModal() {
@@ -112,80 +100,47 @@ this.getItems("");
     this.showfilter = !this.showfilter;
   }
 
-  doRefresh(event) {
-    this.details1 = [];
-    this.assetcode = '';
-    event.target.complete();
-  }
 
 
-  getItems(e:any) {
+
+  getItems(e: any) {
+
     console.log(e);
 
-    this.assetcode1 = [];
-    // if (e.target.value == "") {
-    //   this.assetcode1 = [];
-    //   this.isItemAvailable = false;
-    // };
+    // if (this.strPropertyId == null || this.strPropertyId == undefined) {
+    //   this.strPropertyId = "0"
+    // }
+    // if (this.strPropertyDesc == null || this.strPropertyDesc == undefined) {
+    //   this.strPropertyDesc = "0"
+    // }
+    // //  console.log(this.  );
+    // if (this.strLocationId == null || this.strLocationId == "" || this.strLocationId == undefined) {
+    //   this.strLocationId = "0"
+    // }
+    // if (this.strBranchId == null || this.strBranchId == undefined) {
+    //   this.strBranchId = "0"
+    // }
 
-    const header = new Headers();
-    header.append("Content-Type", "application/json");
-    let options = new HttpHeaders().set('Content-Type', 'application/json');
-    var params = {
-      access_token: window.localStorage['token'],
-      userid: window.localStorage['TUM_USER_ID'],
-      usertoken: window.localStorage['usertoken'],
-      USER_ID: window.localStorage['TUM_USER_ID'],
-    };
-    this.http.get('https://demo.herbie.ai/nTireMobileCoreAPI/api/Property/fm_rental_summary/' + this.strFunctionId + '/' + this.branchid + '/' + this.branchcode
-      + '/' + this.Propertycode + '/' + this.strPropertyDesc + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + this.strusertype + '/' + this.userId, {
-        headers: options,
-      }
-    ).subscribe((resp: any) => {
+
+    // const header = new Headers();
+    // // header.append("Content-Type", "application/json");
+    // let options = new HttpHeaders().set('Content-Type', 'application/json');
+
+
+    // this.http.get('https://demo.herbie.ai/nTireMobileCoreAPI/api/Property/fm_rental_summary/' + this.strFunctionId + '/' + this.branchid + '/' + this.branchcode
+    //   + '/' + this.Propertycode + '/' + this.strPropertyDesc + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + this.strusertype + '/' + this.userId, {
+
+    this.http.get('https://demo.herbie.ai/nTireMobileCoreAPI/api/Property/fm_rental_summary/' + this.strFunctionId + '/' + this.strBranchId + '/' + this.strLocationId
+      + '/' + this.strPropertyId + '/' + this.strPropertyDesc + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + 0 + '/' + this.strusertype + '/' + this.userid, {
+
+    }).subscribe((resp: any) => {
       console.log(resp);
-      
+
       this.propertyCodeResult = resp;
 
-      this.propertyCodeResult1 = this.propertyCodeResult;
-
-      for(var i = 0; i < this.propertyCodeResult1.length; i++ ){
-        this.assetcode1.push(this.propertyCodeResult1[i].ASSET_CODE);
-      };
-      const val = e.target.value;
-
-      if (val && val.trim() != '') {
-        this.isItemAvailable = true;
-        this.assetcode1 = this.assetcode1.filter((item) => {
-          return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-        })
-      }
-    }, error => {
-      //this.presentAlert('Alert','Server Error,Contact not loaded');
-      console.log("error : " + JSON.stringify(error));
     })
 
   };
-
-  // propertyCodE(assetcode:any){
-  //   this.details1 = [];
-  //   this.assetcode = assetcode;
-  //   this.isItemAvailable = false;
-
-  //   var data = {
-  //     'assetcode': assetcode,
-  //     'branchid': this.branchID,
-  //     'access_token': this.accessToken,
-  //     'userid': this.userID,
-  //     'usertoken': this.userToken
-  //   };
-  //   console.log(data);
-
-  //   const header = new Headers();
-  //   header.append("Content-Type", "application/json");
-
-  //   let options = new HttpHeaders().set('Content-Type', 'application/json');
-    
-  // }
 
 
 
