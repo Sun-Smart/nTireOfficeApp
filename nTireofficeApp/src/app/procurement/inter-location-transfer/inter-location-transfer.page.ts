aba37ddc2633c26041f90b2f63873fdcfae906b8
StobartEvansC <113337584+StobartEvansC@users.noreply.github.com>
2022-10-18 18:24:54 +0530

    hrms  and procurement
    hrms
    ==
    open add education select aligment
    ==
    procurment
    ==
    inter location transfer and vendor pending quotation and material request page clear button and function

--- nTireofficeApp/src/app/procurement/inter-location-transfer/inter-location-transfer.page.ts
+++ nTireofficeApp/src/app/procurement/inter-location-transfer/inter-location-transfer.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController, AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-inter-location-transfer',
@@ -6,40 +8,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inter-location-transfer.page.scss'],
 })
 export class InterLocationTransferPage implements OnInit {
-  showlineItems:boolean=true
-  hidelineItems:boolean=false
-  showfilter:boolean=true
-  hidefilter:boolean=true
-  showviewlist:boolean=false
-  trforderno:String;
+  showlineItems: boolean = true
+  hidelineItems: boolean = false
+  showfilter: boolean = true
+  hidefilter: boolean = true
+  showviewlist: boolean = false
+  trforderno: any;
+  Itemcode:any;
+  fromtrfdate:any;
+  totrfdate:any;
+  status:any;
 
 
-  loading:boolean=false
-  constructor() { }
+
+  loading: boolean = false
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
   }
-  showline(){
-    this.showlineItems=!this.showlineItems
-    
+  showline() {
+    this.showlineItems = !this.showlineItems
+
     // this.showfilter = !this.showfilter;
   }
-  close(){
+  close() {
     // this.showviewlist=true
-    this.showlineItems=!this.showlineItems
-    this.hidelineItems=!this.hidelineItems
+    this.showlineItems = !this.showlineItems
+    this.hidelineItems = !this.hidelineItems
 
   }
-  togglefilter(){
+  togglefilter() {
     this.showfilter = !this.showfilter;
     this.hidefilter = !this.hidefilter;
   }
   Search() {
-    this.loading=true
-  
-    this.showviewlist = true
-    if(this.trforderno ==undefined){
-      this.trforderno=''
-    }}
+    this.loading = true
 
+    this.showviewlist = true
+    if (this.trforderno == undefined) {
+      this.trforderno = ''
+    }
+  }
+  submit() {
+    this.showviewlist = true
+    // this.hideviewlist=true
+  }
+
+  async clear() {
+    const alert = await this.alertController.create({
+      header: 'Confirm',
+      message: 'Are you sure want to Cancel the Process',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            this.trforderno = "";
+            this.Itemcode = "";
+            this.fromtrfdate = "";
+            this.totrfdate="";
+            this.status="";
+            // this.prscode = "";
+            // this.status = "";
+            // this.todate = "";
+            // this.fromdate = "";
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
