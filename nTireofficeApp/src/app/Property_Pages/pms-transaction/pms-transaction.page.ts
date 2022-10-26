4af02461a22cdf831d00bdb4abc031062a140dd8
Kalaivanan <kalaivanan@sunsmart.co.in>
2022-10-26 12:21:35 +0530

    Property Changes
    Property Changes

--- nTireofficeApp/src/app/Property_Pages/pms-transaction/pms-transaction.page.ts
+++ nTireofficeApp/src/app/Property_Pages/pms-transaction/pms-transaction.page.ts
@@ -1,6 +1,13 @@
+/* eslint-disable @typescript-eslint/type-annotation-spacing */
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable prefer-const */
+/* eslint-disable no-debugger */
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+/* eslint-disable @typescript-eslint/quotes */
 import { Component, ViewChild, TemplateRef, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ViewEncapsulation } from '@angular/core';
 import { TableSampleService } from '../table-sample.service';
 import { RecieptMasterPagePage } from '../reciept-master-page/reciept-master-page.page';
@@ -18,18 +25,19 @@ export class PmsTransactionPage implements OnInit {
 
   @ViewChild('rowDetailTpl', { static: true }) rowDetailTpl: TemplateRef<any>;
 
-  options = { checkboxes: true }
+  options = { checkboxes: true };
   data: any = [];
   columns: any = [];
   rows: any;
 
-  optionsWithRowDetail = {}
+  optionsWithRowDetail = {};
   dataWithRowDetail = [];
   columnsWithRowDetail: any = [];
   showfilter: boolean = true;
+  branchlist1: any;
 
   constructor(private modalCtrl: ModalController, private http: HttpClient, private tableApi: TableSampleService) {
-
+    this.BranchLocationdata();
     this.columns = [
       { name: 'Name', width: "110", sorting: true },
       { name: 'Company', width: "120" },
@@ -39,13 +47,13 @@ export class PmsTransactionPage implements OnInit {
     this.optionsWithRowDetail = {
       checkboxes: true,
       rowDetailTemplate: this.rowDetailTpl
-    }
+    };
 
   }
 
   ngOnInit() {
     this.dataWithRowDetail = this.tableApi.getData();
-    console.log(this.data)
+    console.log(this.data);
     this.columnsWithRowDetail = [
       { name: 'Name', width: "110", sorting: true },
       { name: 'Company', width: "120" },
@@ -54,7 +62,7 @@ export class PmsTransactionPage implements OnInit {
   }
 
   async transCancel() {
-  await  this.modalCtrl.dismiss('','');
+    await this.modalCtrl.dismiss('', '');
   }
 
   togglefilter() {
@@ -69,5 +77,23 @@ export class PmsTransactionPage implements OnInit {
       component: RecieptMasterPagePage,
     });
     return await model.present();
+  }
+  BranchLocationdata() {
+
+    const header = new Headers();
+    header.append("Content-Type", "application/json");
+
+    let options = new HttpHeaders().set('Content-Type', 'application/json');
+    this.http.get('https://demo.herbie.ai/nTireMobileCoreAPI/api/Property/getbranchid', {
+      headers: options,
+    }).subscribe(resp => {
+      this.branchlist1 = resp;
+      // this.branchlist1 = JSON.parse(this.branchlist1);
+      // console.log("branchlist1 one: " + JSON.stringify(this.branchlist1));
+
+    }, error => {
+
+      console.log("branchlist1 : " + JSON.stringify(error));
+    });
   }
 }
