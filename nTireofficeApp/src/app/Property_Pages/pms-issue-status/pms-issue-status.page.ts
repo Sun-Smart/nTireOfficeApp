3a55fc61ecaad2bf1c25315c37066fda53ff54b3
Muthusamy <muthumaya.12@gmail.com>
2022-10-31 12:21:53 +0530

    pms issue status

--- nTireofficeApp/src/app/Property_Pages/pms-issue-status/pms-issue-status.page.ts
+++ nTireofficeApp/src/app/Property_Pages/pms-issue-status/pms-issue-status.page.ts
@@ -1,5 +1,8 @@
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ModalController } from '@ionic/angular';
+import { IpaddressService } from '../../service/ipaddress.service';
+import { NavParams} from '@ionic/angular';
 
 @Component({
   selector: 'app-pms-issue-status',
@@ -9,12 +12,60 @@ import { ModalController } from '@ionic/angular';
 export class PmsIssueStatusPage implements OnInit {
 
   username = window.localStorage.getItem('TUM_USER_NAME');
+  functionID: any;
+  userID: any;
+  branchID:any;
+  propertyCodeResult: any;
+  branch: any;
+  branchlocation: any;
+  propertycode: any;
+  propertyId: any;
+  getStatus: any;
+  propertyID:any;
+  viewStatus: any;
   
-  constructor(private modalCtrl: ModalController) { }
+  constructor(private modalCtrl: ModalController,
+    public alertController: AlertController,
+    private http: HttpClient,
+    public Ipaddressservice: IpaddressService, private navParams: NavParams) { }
 
   ngOnInit() {
+    this.viewStatus =  this.navParams.get('Data');
+    console.log(this.viewStatus);
+    this.functionID = localStorage.getItem('FUNCTION_ID');
+    this.userID = localStorage.getItem('TUM_USER_ID');
+    this.branchID = localStorage.getItem('TUM_BRANCH_ID');
+
+    this.getIssueStatus();
   }
-  cancel(){
-    return this.modalCtrl.dismiss(null, 'cancel');
-  }
+
+
+getIssueStatus(){
+
+this.propertyId = this.viewStatus.property_id;
+
+console.log(this.propertyId);
+
+  
+
+  const header = new Headers();
+  header.append("Content-Type", "application/json");
+
+  let options = new HttpHeaders().set('Content-Type', 'application/json');
+  this.http.get(this.Ipaddressservice.ipaddress + this.Ipaddressservice.serviceurlProperty + 'getissuestatus/' + this.functionID + '/' + this.branchID + '/' + this.propertyId, {
+    headers : options,
+  }).subscribe(resp =>{
+      this.getStatus = resp;
+
+      console.log(this.getStatus);
+      
+  });
+};
+
+
+
+
+cancel(){
+  return this.modalCtrl.dismiss(null, 'cancel');
+}
 }
