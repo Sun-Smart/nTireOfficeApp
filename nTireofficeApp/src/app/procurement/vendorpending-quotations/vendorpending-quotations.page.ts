341efbecca4829cc0cae17e55425ccf1c3096fc7
suneel-M <suneel@sunsmart.co.in>
2022-11-01 10:20:36 +0530

    vendorpending functionlity work design
    vendorpending functionlity work design

--- nTireofficeApp/src/app/procurement/vendorpending-quotations/vendorpending-quotations.page.ts
+++ nTireofficeApp/src/app/procurement/vendorpending-quotations/vendorpending-quotations.page.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActionSheetController, AlertController } from '@ionic/angular';
+import { IpaddressService } from 'src/app/service/ipaddress.service';
 
 @Component({
   selector: 'app-vendorpending-quotations',
@@ -7,14 +9,17 @@ import { ActionSheetController, AlertController } from '@ionic/angular';
   styleUrls: ['./vendorpending-quotations.page.scss'],
 })
 export class VendorpendingQuotationsPage implements OnInit {
-  rfocode:any;
-  fromdate:any;
-  todate:any;
-  status:any;
-  Quotation:any;
-  constructor(private alertController: AlertController) { }
+  rfqcode: any;
+  fromdate: any;
+  todate: any;
+  status: any;
+  Quotation: any;
+  getresponse: any;
+  constructor(private alertController: AlertController, private httpclient: HttpClient, private Ipaddressservice: IpaddressService) { }
 
   ngOnInit() {
+
+
   }
   async clear() {
     const alert = await this.alertController.create({
@@ -31,18 +36,60 @@ export class VendorpendingQuotationsPage implements OnInit {
         }, {
           text: 'Yes',
           handler: () => {
-
-            this.rfocode = "";
+            this.rfqcode = "";
             this.fromdate = "";
             this.todate = "";
             this.status = "";
-            this.Quotation="";
+            this.Quotation = "";
           }
         }
       ]
     });
 
     await alert.present();
+  }
+
+
+  Submit() {
+
+    if (this.rfqcode = undefined) {
+      this.rfqcode = 0
+    }
+    if (this.status = undefined) {
+      this.status = 0
+    }
+    if (this.fromdate = undefined) {
+      this.fromdate = 0
+    }
+    if (this.rfqcode = undefined) {
+      this.rfqcode = 0
+    }
+
+    var body = {
+      "FUNCTIONIDP": "1",
+      "BRANCHIDP": "1",
+      "RFQCODEP": this.rfqcode,
+      "FROMDATEP": this.fromdate,
+      "TODATEP": this.todate,
+      "ITEMCODEP": "",
+      "VENDORIDP": "",
+      "STATUSP": this.status,
+      "PAGEINDEXP": 0,
+      "PAGESIZEP": 0,
+      "SORTEXPRESSIONP": "",
+      "ALPHANAMEP": "",
+      "modep": ""
+    };
+
+    this.httpclient.post(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceerpapi + 'get_pending_quotation_search', body).subscribe((res: any) => {
+      this.getresponse = res;
+      console.log("Response", res)
+      console.log("Response", res)
+      for (let item of this.getresponse) {
+        console.log(item);
+      }
+    })
+
   }
 
 }
