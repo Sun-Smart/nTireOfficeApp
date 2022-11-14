e47bee793ac9974d4dac12ecab2fc184ed422d36
suriya1517 <suriya@sunsmart.co.in>
2022-11-14 19:53:03 +0530

    view invoice api integration

--- nTireofficeApp/src/app/procurement/view-invoice-status/view-invoice-status.page.ts
+++ nTireofficeApp/src/app/procurement/view-invoice-status/view-invoice-status.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { IpaddressService } from '../../service/ipaddress.service';
 @Component({
   selector: 'app-view-invoice-status',
   templateUrl: './view-invoice-status.page.html',
@@ -16,13 +17,31 @@ export class ViewInvoiceStatusPage implements OnInit {
   status: any;
   fromDate: any;
   Paymentref: any;
-  constructor(private alertController: AlertController) {
+  branchID: string;
+  functionID: string;
+  userID: string;
+  usertype: string;
+  accessToken: string;
+  invoiceget: any;
+  poinvoive_no: any;
+  poinvoicecode: any=[];
+  isPropertycodeAvailable: boolean=false;
+  ponumber: any;
+  poid: any;
+  constructor(private alertController: AlertController, public Ipaddressservice: IpaddressService,  private http: HttpClient,) {
     this.dat_valid = {
       currentDate: new Date()
     };
+
+    this.branchID = localStorage.getItem('TUM_BRANCH_ID');
+    this.functionID = localStorage.getItem('FUNCTION_ID');
+    this.userID = localStorage.getItem('TUM_USER_ID');
+    this.usertype = localStorage.getItem('TUM_USER_TYPE');
+    this.accessToken = localStorage.getItem('token');
   }
 
   ngOnInit() {
+    this.getinvoice();
   }
   close() {
 
@@ -58,4 +77,81 @@ export class ViewInvoiceStatusPage implements OnInit {
 
     await alert.present();
   }
+
+
+getinvoice(){
+  let options = new HttpHeaders().set('Content-Type', 'application/json');
+  this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceerpapi + 'getinvoicedetails/' +  this.functionID + "/" +     this.branchID + "/" + 0, {
+    headers: options,
+  }).subscribe(resp => {
+    this.invoiceget = resp;
+ 
+  });
+}
+
+
+
+
+getinvoiceCode(e:any){
+
+
+  this.poinvoicecode = [];
+  if (e.target.value == "") {
+    this.poinvoicecode = [];
+    this.isPropertycodeAvailable = false;
+  }
+  const header = new Headers();
+  header.append("Content-Type", "application/json");
+  let options = new HttpHeaders().set('Content-Type', 'application/json');
+  this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceerpapi + 'getponnumber/' +this.functionID+ "/" +  this.branchID + "/" + e.target.value, {
+  
+  }).subscribe(resp => {
+    this.poinvoicecode = [];
+    this.isPropertycodeAvailable = false;
+
+this.poinvoive_no=resp
+for (var i = 0; i < this.poinvoive_no.length; i++) {
+  this.poinvoicecode.push({
+    po_number: this.poinvoive_no[i].po_number,
+      
+  });
+};
+
+const val = e.target.value;
+if (val && val.trim() != '') {
+  this.isPropertycodeAvailable = true;
+  this.poinvoicecode = this.poinvoicecode.filter((item) => {
+    return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+  });
+}
+
+  })
+}
+
+
+
+
+
+
+addponumbercode(item:any){
+  console.log(item,"item");
+  this.poid=item.po_id;
+  this.ponumber=item.po_number;
+  this.isPropertycodeAvailable = false;
+
+}
+
+
+
+
+filterinvoicelist(){
+  let options = new HttpHeaders().set('Content-Type', 'application/json');
+  this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceerpapi + 'getinvoicedetails/' +  this.functionID + "/" +     this.branchID + "/" + this.poid, {
+    headers: options,
+  }).subscribe(resp => {
+    this.invoiceget = resp;
+ 
+  });
+}
+
 }
