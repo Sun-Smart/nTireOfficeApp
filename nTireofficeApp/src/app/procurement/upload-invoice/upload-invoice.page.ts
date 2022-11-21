ec1ca887e887dee7b614c6b9477b42fdad29dae3
suriya1517 <suriya@sunsmart.co.in>
2022-11-21 13:01:12 +0530

    upload invoice and vendorpo conform pages api integration

--- nTireofficeApp/src/app/procurement/upload-invoice/upload-invoice.page.ts
+++ nTireofficeApp/src/app/procurement/upload-invoice/upload-invoice.page.ts
@@ -3,6 +3,8 @@ import { AlertController, LoadingController } from '@ionic/angular';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { InAppBrowser, InAppBrowserOptions } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { IpaddressService } from '../../service/ipaddress.service';
 
 import { Platform } from '@ionic/angular';
 
@@ -21,6 +23,7 @@ export class UploadInvoicePage implements OnInit {
 
   isOpen = false;
   public invoiceform = new FormGroup({
+    pocode:new FormControl(''),
     invoicenumber: new FormControl('', Validators.compose([Validators.required])),
     fromDate: new FormControl(''),
     invoiceamount: new FormControl(''),
@@ -48,7 +51,17 @@ export class UploadInvoicePage implements OnInit {
  
   windowop: any;
   filePath: string;
-  constructor( public alertController: AlertController,private modalCtrl: ModalController,private readonly iab: InAppBrowser, public platform: Platform) {
+  branchID: string;
+  functionID: string;
+  userID: string;
+  usertype: string;
+  accessToken: string;
+  poinvoicecode: any[];
+  isPropertycodeAvailable: boolean;
+  poinvoive_no: any;
+  poid: any;
+  ponumber: any;
+  constructor( public alertController: AlertController,private modalCtrl: ModalController,private readonly iab: InAppBrowser, public platform: Platform , public Ipaddressservice: IpaddressService,  private http: HttpClient) {
 
     this.dat_valid = {
       currentDate: new Date()
@@ -56,6 +69,11 @@ export class UploadInvoicePage implements OnInit {
 
     };
 
+    this.branchID = localStorage.getItem('TUM_BRANCH_ID');
+    this.functionID = localStorage.getItem('FUNCTION_ID');
+    this.userID = localStorage.getItem('TUM_USER_ID');
+    this.usertype = localStorage.getItem('TUM_USER_TYPE');
+    this.accessToken = localStorage.getItem('token');
     // this.Formdata={
 
     // }
@@ -83,9 +101,75 @@ export class UploadInvoicePage implements OnInit {
     await alert.present();
   }
 
+
+  async presentAlert1(heading, tittle) {
+    var alert = await this.alertController.create({
+      header: heading,
+      cssClass: 'Cssbutton',
+      backdropDismiss: false,
+      message: tittle,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  };
+
   get f() {
     return this.invoiceform.controls;
   }
+
+
+  getinvoiceCode(e:any){
+
+
+    this.poinvoicecode = [];
+    if (e.target.value == "") {
+      this.poinvoicecode = [];
+      this.isPropertycodeAvailable = false;
+    }
+    // const header = new Headers();
+    // header.append("Content-Type", "application/json");
+     let options = new HttpHeaders().set('Content-Type', 'application/json');
+    this.http.get(this.Ipaddressservice.ipaddress1 + this.Ipaddressservice.serviceerpapi + 'getponnumber/' +this.functionID+ "/" +  this.branchID + "/" + e.target.value, {
+      headers: options,
+    }).subscribe(resp => {
+      this.poinvoicecode = [];
+      this.isPropertycodeAvailable = false;
+  
+  this.poinvoive_no=resp
+  for (var i = 0; i < this.poinvoive_no.length; i++) {
+    this.poinvoicecode.push({
+      po_number: this.poinvoive_no[i].po_number,
+        
+    });
+  };
+  
+  const val = e.target.value;
+  if (val && val.trim() != '') {
+    this.isPropertycodeAvailable = true;
+    this.poinvoicecode = this.poinvoicecode.filter((item) => {
+      return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+    });
+  }
+  
+    })
+  }
+  
+  
+  
+  
+  
+  
+  addponumbercode(item:any){
+    console.log(item,"item");
+    this.poid=item.po_id;
+    this.ponumber=item.po_number;
+    this.isPropertycodeAvailable = false;
+  
+  }
+
+
+
   additem() {
 
 if(this.invoiceform.value.invoicenumber==""|| this.invoiceform.value.invoicenumber=="undefined" || this.invoiceform.value.invoicenumber==null){
@@ -101,52 +185,75 @@ if(this.invoiceform.value.invoicenumber==""|| this.invoiceform.value.invoicenumb
   }
 else{
 
+let data={
+  "filename": this.filename,
+  "functionid": this.functionID ,
+  "branchid": this.branchID,
+  "poid":this.poid,
+  "invoicedate":this.invoiceform.value.fromDate,
+  "invoiceref":"INN001",
+  "invoiceamount":this.invoiceform.value.invoiceamount,
+  "remarks":this.invoiceform.value.invoiceremark,
+  "userid": this.userID ,
+  "filedata":this.pdfdata
 
-      this.invoicedata = [
-        {
-          invoicenumber: this.invoiceform.value.invoicenumber,
-          fromDate: this.invoiceform.value.fromDate,
-          invoiceamount: this.invoiceform.value.invoiceamount,
-          documentupload: this.filename,
-          invoiceremark: this.invoiceform.value.invoiceremark,
-        }
-      ]
+}
 
-      console.log(this.invoicedata, 'formvalue')
-
-
-      this.cardshow = true
-
-      // this.getinvoice = this.arr
-      console.log(this.getinvoice);
-
-
-
-      for (let i = 0; i < this.invoicedata.length; i++) {
-        this.getinvoice.push(this.invoicedata[i]);
-        console.log(this.getinvoice);
-
-        // this.getinvoice = this.arr[i];
-      }
-      this.invoiceform.reset();
+this.http.post(this.Ipaddressservice.ipaddress + this.Ipaddressservice.serviceerpapi + 'uploadinvoice/', data).subscribe(res => {
+if(res="Uploaded successfully"){
+  this.presentAlert1("add item Success",'Insert Successfully');
+}
+})
 
 
-   this.pdfdata=this.url.slice(28)
 
- console.log( this.pdfdata);
- const byteArray = new Uint8Array(
-   atob( this.pdfdata)
-     .split("")
-     .map(char => char.charCodeAt(0))
- );
- const file = new Blob([byteArray], { type: "application/pdf" });
- const fileURL = URL.createObjectURL(file);
- var obj = {
-   test:this.url,
-   test2:this.invoicedata[0].invoicenumber,
- }
- this.pdfSrc.push(obj);
- console.log(this.pdfSrc,"pdfsrc")
+
+
+//       this.invoicedata = [
+//         {
+//           invoicenumber: this.invoiceform.value.invoicenumber,
+//           fromDate: this.invoiceform.value.fromDate,
+//           invoiceamount: this.invoiceform.value.invoiceamount,
+//           documentupload: this.filename,
+//           invoiceremark: this.invoiceform.value.invoiceremark,
+//         }
+//       ]
+
+//       console.log(this.invoicedata, 'formvalue')
+
+
+//       this.cardshow = true
+
+//       // this.getinvoice = this.arr
+//       console.log(this.getinvoice);
+
+
+
+//       for (let i = 0; i < this.invoicedata.length; i++) {
+//         this.getinvoice.push(this.invoicedata[i]);
+//         console.log(this.getinvoice);
+
+//         // this.getinvoice = this.arr[i];
+//       }
+//       this.invoiceform.reset();
+
+
+//    this.pdfdata=this.url.slice(28)
+
+//  console.log( this.pdfdata);
+//  const byteArray = new Uint8Array(
+//    atob( this.pdfdata)
+//      .split("")
+//      .map(char => char.charCodeAt(0))
+//  );
+//  const file = new Blob([byteArray], { type: "application/pdf" });
+//  const fileURL = URL.createObjectURL(file);
+//  var obj = {
+//    test:this.url,
+//    test2:this.invoicedata[0].invoicenumber,
+//  }
+//  this.pdfSrc.push(obj);
+//  console.log(this.pdfSrc,"pdfsrc")
   }
 }
 
@@ -165,6 +272,7 @@ onSelectFile(event) {
     reader.onload = (event) => { // called once readAsDataURL is completed
       this.url = event.target.result;
     }
+    this.pdfdata=this.url.slice(28)
     // reader.readAsArrayBuffer($img.files[0]);
     // console.log( $img.files[0]);
     // this.pdfimg= $img.files[0]
@@ -177,150 +285,58 @@ onSelectFile(event) {
  
 }
 
-// openPDF (stringBase64PDF) {
-//   debugger
-//   fetch('data:application/pdf;base64,' + stringBase64PDF, {
-//       method: "GET"
+
+
+
+
+// preview pfd
+
+// openpdf(invoiceNumber){
+//  debugger
+//   var test = this.pdfSrc.filter(src=> {
+//     debugger
+//     if(src.test2 == invoiceNumber) {
+//       debugger
+//       return src.test;
+//     }
 //   })
-//   .then(res => res.blob()).then(blob => {
-//     console.log("created blob");
-//     this.file.createFile(this.file.dataDirectory, 'temp.pdf', true)
-//     .then(() => {
-//       console.log("file created");
-//       this.file.writeFile(this.file.dataDirectory, 'temp.pdf', blob, { replace: true })
-//       .then(res => {
-//         console.log("file writed");
-//         this.fileOpener.open(res.toInternalURL(), 'application/pdf')
-//         .then((res) => {
-//           console.log('file opened')
-//         }).catch(err => {
-//           console.log('open error')
-//         });
-//       }).catch(err => {
-//         console.log('write error')     
-//       });
-//     }).catch(() => {
-//       console.log("create error");
-//     })
-    
-//   }).catch(err => {
-//     console.log('blob error')
-//   });
-// }
 
 
-
-
-openpdf(invoiceNumber){
- debugger
-  var test = this.pdfSrc.filter(src=> {
-    debugger
-    if(src.test2 == invoiceNumber) {
-      debugger
-      return src.test;
-    }
-  })
-  // this.iab.create('https://ionicframework.com/');
-  // window.open(test[0].test)
-
- this.open=test[0].test
+//  this.open=test[0].test
  
-//  localStorage.setItem('open',this.open)
-//  this.getopen=localStorage.getItem("open")
- console.log(this.open);
- this.openfile=this.open.slice(5)
- window.PreviewAnyFile.previewBase64(
-   success=>console.log("on success",success),
-  error=>console.log("on error", error),
-  this.open
+//  console.log(this.open);
+//  this.openfile=this.open.slice(5)
+//  window.PreviewAnyFile.previewBase64(
+//    success=>console.log("on success",success),
+//   error=>console.log("on error", error),
+//   this.open
    
- )
-// window.open(this.open +'.pdf');
-//  console.log(window.open);
-//  var pathFile = "";
-//  var fileName ='PdfName.pdf';
-// var contentFile =  this.open;
-// // var contentType = "application/pdf";
-// if (this.platform.is('ios')) {
-//      pathFile = cordova.file.documentsDirectory
-// } else {
-//      pathFile = cordova.file.externalRootDirectory
+//  )
 // }
-//         let filePath = (this.platform.is('android')) ? 
-//         this.file.externalRootDirectory : this.file.cacheDirectory;
-//         this.file.createFile(filePath, fileName, true)
-//         .then(() => {
-//   this.file.writeFile(filePath, fileName, contentFile,{ replace: true }).then(FileEntry => {
-// console.log(FileEntry);
-
-//   console.log("File created!");          
-//   this.fileOpener.open(FileEntry.toURL(), 'application/pdf')
-//     .then(() => console.log('File is opened'))
-//     .catch(err => console.error('Error openening file: ' + err));
-// })
-// })
-//   .catch((err) => {
-//     console.error("Error creating file: " + err);
-//     throw err;  
-//   });
-
-
-
-
-//  console.log(this.openfile);
-//  this.fileOpener.open( this.open, 'application/pdf')
-// let options: InAppBrowserOptions = {
-//   location: 'yes',
-//   hideurlbar: 'yes',
-//   hidenavigationbuttons: 'yes',
-//   clearcache: 'no',
-//   clearsessioncache: 'yes',
-//   closebuttoncaption: 'Close',
-//   zoom: 'no',
-//   closebuttoncolor: '#888888',
-//   height:"300px",
-//   width:"200px"
-// };
-
-
-
-// RESULT_BROWSER.show();
-//  html2pdf()
-//  .set(opt)
-// html2pdf().set(opt).from(element).topdf().get('pdf').then(pdf=>{
-//   this.iab.create(pdf.output("bloburl"));
-
-// })
-// this.iab.create(this.open);
-  // console.log(this.pdfSrc);
-
-  // for (let i = 0; i < this.pdfSrc.length; i++) {
-          
-  //         this.fest.push(this.invoicedata[i]);
-    // console.log(this.getinvoice);
-
-    // this.getinvoice = this.arr[i];
-  // }
-}
-
-
-
-
-// async createModal() {
-//   const model = await this.modalCtrl.create({
-//     component: PdfModalPage,
-//     componentProps: { Data:this.url }
-//   });
-//   return await model.present();
-// };
 
 
 
 
 
-removeItem(index : number){
-  this.getinvoice.splice(index,1);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// removeItem(index : number){
+//   this.getinvoice.splice(index,1);
+// }
 
 
 
--- nTireofficeApp/src/app/procurement/vendorpoconfirm/vendorpoconfirm.module.ts
+++ nTireofficeApp/src/app/procurement/vendorpoconfirm/vendorpoconfirm.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { VendorpoconfirmPageRoutingModule } from './vendorpoconfirm-routing.module';
 import { VendorpoconfirmPage } from './vendorpoconfirm.page';
-
+import { DatePipe } from '@angular/common';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     VendorpoconfirmPageRoutingModule,
 
   ],
-  declarations: [VendorpoconfirmPage]
+  declarations: [VendorpoconfirmPage],
+  providers:[DatePipe]
 })
 export class VendorpoconfirmPageModule {}
