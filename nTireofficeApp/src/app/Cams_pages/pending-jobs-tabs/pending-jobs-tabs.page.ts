ee2cd8fde2ca173718a929c27769886c3f7b22bf
Samundeeswari-Pandian <113327771+Samundeeswari-Pandian@users.noreply.github.com>
2022-12-21 14:35:50 +0530

    CAMS Issue correction
    CAMS Issue correction

--- nTireofficeApp/src/app/Cams_pages/pending-jobs-tabs/pending-jobs-tabs.page.ts
+++ nTireofficeApp/src/app/Cams_pages/pending-jobs-tabs/pending-jobs-tabs.page.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
-import {TabparamserviceService} from '../../service/tabparamservice.service'
-
+import {TabparamserviceService} from '../../service/tabparamservice.service';
 @Component({
   selector: 'app-pending-jobs-tabs',
   templateUrl: './pending-jobs-tabs.page.html',
   styleUrls: ['./pending-jobs-tabs.page.scss'],
 })
 export class PendingJobsTabsPage implements OnInit {
-  
-  constructor(private Tabparams:TabparamserviceService) { 
-   
+
+  constructor(private Tabparams:TabparamserviceService) {
+
   }
 
   ngOnInit() {
